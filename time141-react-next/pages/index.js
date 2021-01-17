a34980fba0a09bd3600d64d93e055348e0bccba4
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-01-17 18:22:12 -0300

    Update index.js

--- time141-react-next/pages/index.js
+++ time141-react-next/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import ListServico from '../componets/ListServico'
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
       
       <div>
-        Começamos 
+        <ListServico />
       </div>
       
     </div>

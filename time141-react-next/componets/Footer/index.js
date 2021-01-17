5565329cbe9b86c35aef2b016096633afc56d30f
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-01-17 19:18:18 -0300

    Update index.js

--- time141-react-next/componets/Footer/index.js
+++ time141-react-next/componets/Footer/index.js
@@ -7,9 +7,9 @@ function Footer() {
             <nav className="md:hidden  space-x-8">
             <ul className="space-x-8">
                         <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/">Home</Link></il>
-                        <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/pedidos">Pedidos</Link></il>
+                        <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/pedidos">Avaliações</Link></il>
                         <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/perfil">Perfil</Link></il>
-                        <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/cestinha">Cestinha</Link></il>
+                        <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/cestinha">Pontos</Link></il>
             </ul>
             </nav>
         </footer>

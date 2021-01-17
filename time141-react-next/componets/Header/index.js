6d00af2201b4abac8f2928e6c182daba5b08285c
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-01-17 18:19:11 -0300

    Create index.js

---
+++ time141-react-next/componets/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+
+function Header() {
+    return(
+        <div className="w-full h-15 bg-blue-300  ">
+            <header className="flex  md:justify-between max-w-5xl mx-auto py-2 px-4 sm:px-6 md:px-8 md:py-4 items-center ">
+               <a className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/endereco">Seu endereço</Link></a>
+
+                <nav className="hidden md:block ">
+                    <ul className="space-x-8">
+                        <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/">Home</Link></il>
+                        <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/pedidos">Pedidos</Link></il>
+                        <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/perfil">Perfil</Link></il>
+                        <il className="tracking-wide hover:text-gray-600 ml-4 text-white font-semibold"><Link  href="/cestinha">Cestinha</Link></il>
+                    </ul>
+                    
+                </nav>
+            </header>
+        </div>
+        
+    );
+}
+
+export default Header;

1a8c2353a7902b679860a423eed8427199473458
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-01-17 16:03:51 -0300

    Configuração inicial

---
+++ time141-react-next/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>App time 141</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <div>
+        Começamos 
+      </div>
+      
+    </div>
+  )
+}
